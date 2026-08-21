acf12b1480b8fbdbf94d0f1dbdcd3f664adae5c0
Matthew Nguyen <m.nguyen2720@gmail.com>
2026-08-21 06:47:55 -0700

    Initial project commit

---
+++ kiltersql.py
@@ -0,0 +1,10 @@
+"""
+Kilter Grade Predictor - ML Model
+
+Predict a climb's V-grade from its hold layout on the original Kilter Board.
+Currently only working on the 7x10 layout for more simplicity.
+First iteration will be a XGBoost regression model. The second iteration will be a either a GNN or CNN model.
+
+"""
+
+import sqlite3
